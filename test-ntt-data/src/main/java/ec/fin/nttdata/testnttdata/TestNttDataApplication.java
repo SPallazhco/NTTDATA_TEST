1109c7ceb5af83de33d17ab5cd1629f836713be2
SPallazhco <67174857+SPallazhco@users.noreply.github.com>
2022-08-27 22:51:59 -0500

    Add files via upload
    Creacion de repositorio test de microservicio transaccional

---
+++ test-ntt-data/src/main/java/ec/fin/nttdata/testnttdata/TestNttDataApplication.java
@@ -0,0 +1,13 @@
+package ec.fin.nttdata.testnttdata;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestNttDataApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TestNttDataApplication.class, args);
+    }
+
+}
---
+++ test-ntt-data/src/test/java/ec/fin/nttdata/testnttdata/TestNttDataApplicationTests.java
@@ -0,0 +1,13 @@
+package ec.fin.nttdata.testnttdata;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestNttDataApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
